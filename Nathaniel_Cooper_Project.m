0274c5409afca50724faccc417be7aca0c30193d
nmcooper <nmcooper@ucsc.edu>
2014-05-17 19:13:04 -0700

    setting params

--- Nathaniel_Cooper_Project.m
+++ Nathaniel_Cooper_Project.m
@@ -1,1 +1,11 @@
 %% Nathaniel Cooper project
+
+% Paramators
+rk=0.5 % growth rate of species K
+ak=0.2 % capture efficancy of species K
+
+rd=0.5 % growth rate of species D
+ad=0.5 % capture efficancy of species D
+
+
+
