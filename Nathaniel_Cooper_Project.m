6bca12490e7e0eb2660773f0ea3ebc700775acaa
nmcooper <nmcooper@ucsc.edu>
2014-05-17 19:19:50 -0700

    all params initalized

--- Nathaniel_Cooper_Project.m
+++ Nathaniel_Cooper_Project.m
@@ -7,5 +7,14 @@ ak=0.2 % capture efficancy of species K
 rd=0.5 % growth rate of species D
 ad=0.5 % capture efficancy of species D
 
+bk=0.2 % conversion efficancy of prey K
+qg=0.4 % death rate of species G
+
+bd=0.2 % conversion efficeancy of prey D
+qc=0.4 % death rate of species C
+
+bc=0.5 % conversion efficeancy of prey C
+bg=0.5 % conversion efficeancy of prey G
+qa=0.5 % death rate of species A
 
 
