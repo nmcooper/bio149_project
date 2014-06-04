429080fa0368806aa7a5b540704ce8fa0b3b7e1a
nmcooper <nmcooper@ucsc.edu>
2014-06-04 10:17:03 -0700

    stable population with carying capacity at t=700

--- Nathaniel_Cooper_Project.m
+++ Nathaniel_Cooper_Project.m
@@ -1,19 +1,19 @@
 %% Nathaniel Cooper project
 % with no prey selection variablity in the top preditor
 % Paramters
-time=1:100; % time span in years
+time=1:1000; % time span in years
 
-rk=0.33; % growth rate of species K calculated from  loss estmate
+rk=0.5; % growth rate of species K calculated from  loss estmate
 ak=0.44; % capture efficancy of species K (best logical guess)
 
-rd=0.33; % growth rate of species D calculated from  loss estmate
+rd=0.5; % growth rate of species D calculated from  loss estmate
 ad=0.44; % capture efficancy of species D (best logical guess)
 
-bk=0.10; % conversion efficancy of prey K (best logical guess)
+bk=0.65; % conversion efficancy of prey K (best logical guess)
 ag=0.305; % capture efficancy of species G (Ostfeld 1982)
 qg=0.01; % death rate of species G (beast logical estament)
 
-bd=0.10; % conversion efficeancy of prey D (best logical guess)
+bd=0.65; % conversion efficeancy of prey D (best logical guess)
 ac=0.267; % capture efficeancy of species C (Ostfeld 1982)
 qc=0.01; % death rate of species C (beast logical estament)
 
@@ -21,7 +21,6 @@ bc=0.25; % conversion efficeancy of prey C normilized and calculated from (Ostfe
 bg=0.75; % conversion efficeancy of prey G normilized and calculated from (Ostfeld 1982)
 qa=0.23; % death rate of species A, from the number of documented staranding (Estes and Hatfield 2003)
  
-L=500; % carrining capacity
 
 % dK/dt = rk*K-ak*K*G;
 % dD/dt = rd*D-ad*D*C;
@@ -29,7 +28,7 @@ L=500; % carrining capacity
 % dC/dt = (bd*ad*D*C-qc*C)-ac*C*A;
 % dA/dt = (bc*ac*C*A+bg*ag*G*A)-qa*A;
 
-n0=[555;555;60;60;1]; % starting pop size with biomass at next level calculated from (Reed and Foster 1984) and (Ostfeld 1982)
+n0=[700;700;70;70;2]; % starting pop size with biomass at next level calculated from (Reed and Foster 1984) and (Ostfeld 1982)
 
 eq=0; % extinction level of population
 
@@ -73,12 +72,10 @@ L=500; % carrining capacity
 
 n0=[555;555;60;60;1]; % starting pop size with biomass at next level calculated from (Reed and Foster 1984) and (Ostfeld 1982)
 
-
-
 eq=0;
 
 for i=1:50 % solve ODE every seven days for 100 years 52 weeks * 100=5200 weekes
-   p=betarnd(0.5,0.5); % capture efficeancy of prey C; A=0.5 B=0.5
+   p=betarnd(4,2); % capture efficeancy of prey C; A=0.5 B=0.5
    ac=p;
    ag=ac*(1-p); % capture efficeancy of prey G
 [T, Y] = ode45(@(t,y) LV_Predcom(y,rk,rd,ak,ad,ag,ac,bk,bd,bc,bg,qg,qc,qa),time,n0); % Solve ODE
