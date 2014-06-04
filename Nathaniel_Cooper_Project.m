fe54368d2522013391f43fdccfda8ea67f62a8db
nmcooper <nmcooper@ucsc.edu>
2014-06-04 10:36:09 -0700

    new section with stable population

--- Nathaniel_Cooper_Project.m
+++ Nathaniel_Cooper_Project.m
@@ -1,5 +1,6 @@
 %% Nathaniel Cooper project
-% with no prey selection variablity in the top preditor
+% with no prey selection variablity in the top preditor (find stable
+% population levels
 % Paramters
 time=1:1000; % time span in years
 
@@ -39,11 +40,51 @@ figure;
 plot(T,Y);
 xlabel('time'); ylabel(' Abundance of preditors and prey');
 legend({'K,abundance','D,abundance', 'G, abundance','C,abundance','A,abundance'});
+%% Stable population (no varabliity)
+clear all 
+time=1:1000; % time span in years
 
+rk=0.5; % growth rate of species K calculated from  loss estmate
+ak=0.44; % capture efficancy of species K (best logical guess)
+
+rd=0.5; % growth rate of species D calculated from  loss estmate
+ad=0.44; % capture efficancy of species D (best logical guess)
+
+bk=0.65; % conversion efficancy of prey K (best logical guess)
+ag=0.305; % capture efficancy of species G (Ostfeld 1982)
+qg=0.01; % death rate of species G (beast logical estament)
+
+bd=0.65; % conversion efficeancy of prey D (best logical guess)
+ac=0.267; % capture efficeancy of species C (Ostfeld 1982)
+qc=0.01; % death rate of species C (beast logical estament)
+
+bc=0.25; % conversion efficeancy of prey C normilized and calculated from (Ostfeld 1982)
+bg=0.75; % conversion efficeancy of prey G normilized and calculated from (Ostfeld 1982)
+qa=0.23; % death rate of species A, from the number of documented staranding (Estes and Hatfield 2003)
+ 
+
+% dK/dt = rk*K-ak*K*G;
+% dD/dt = rd*D-ad*D*C;
+% dG/dt = (bk*ak*K*G-qg*G)-ag*G*A;
+% dC/dt = (bd*ad*D*C-qc*C)-ac*C*A;
+% dA/dt = (bc*ac*C*A+bg*ag*G*A)-qa*A;
+
+n0=[325;384;0.81;0.84;304]; % satble population levels 
+
+eq=0; % extinction level of population
+
+[T, Y] = ode45(@(t,y) LV_Predcom(y,rk,rd,ak,ad,ag,ac,bk,bd,bc,bg,qg,qc,qa),time,n0); % Solve ODE    
+
+
+figure;
+plot(T,Y);
+xlabel('time'); ylabel(' Abundance of preditors and prey');
+legend({'K,abundance','D,abundance', 'G, abundance','C,abundance','A,abundance'});
 %%
 % with prey selection varabilty in top preditor using a beta distrabution
 clear all
 % Paramters
+
 time=1:50; % time span in years
 
 rk=0.33; % growth rate of species K calculated from  loss estmate
